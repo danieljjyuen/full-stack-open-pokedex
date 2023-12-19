40002784f676901932a6b67fc7df72b9bcd544fc
danieljjyuen <dan@LAPTOP-AP8VLLC3.localdomain>
2023-12-19 13:29:43 -0500

    added health check endpoints

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
+app.get('/version', (req, res) => {
+  res.send(version)
+})
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
